7d2a6ae30860e4c14fcad6aa46c159b5d448a95c
twrkkk <zhenya.shein.03@gmail.com>
2023-02-16 22:53:26 +0300

    1

---
+++ Practice/Multimap frequency dictionary/multimap frequency dictionary.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <map>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+std::multimap<char, int> freq_dict(string word)
+{
+	std::multimap<char, int> result;
+	int size = word.size();
+	std::multimap<char, int>::iterator it;
+	for (int i = 0; i < size; i++)
+	{
+		it = result.find(word[i]);
+		if (it == result.end())
+			result.insert(std::pair<char, int>(word[i], 1));
+		else
+			++it->second;
+	}
+	return result;
+}
+
+int main()
+{
+	string word;
+	cin >> word;
+	std::multimap<char, int> map = freq_dict(word);
+	for(auto pair: map)
+		cout << pair.first << ' ' << pair.second << '\n';
+}
