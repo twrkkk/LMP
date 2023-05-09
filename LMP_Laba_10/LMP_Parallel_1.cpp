cfe2981284de5c0cf67418533949e19ed27f11b0
twrkkk <zhenya.shein.03@gmail.com>
2023-05-09 17:31:10 +0300

    f

--- LMP_Laba_10/LMP_Parallel_1.cpp
+++ LMP_Laba_10/LMP_Parallel_1.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <Windows.h>
 #include <process.h>
+#include <algorithm>
 
 const size_t N = 9;
 const size_t NTHREAD = 3;
@@ -18,7 +19,7 @@ bool is_sorted_non_parallel(int* a)
 {
 	int i = 0;
 	bool flag = true;
-	while (i<N - 1 && a[i + 1] > a[i])
+	while (i < N - 1 && a[i + 1] >= a[i])
 		++i;
 	if (i != N - 1)
 		flag = false;
@@ -32,13 +33,13 @@ unsigned __stdcall sorted_task(void* a)
 
 	int i = inform->beg;
 	bool flag = true;
-	while (i<inform->end && inform->a[i + 1] > inform->a[i])
+	while (i < inform->end && inform->a[i + 1] >= inform->a[i])
 		++i;
 	if (i != inform->end)
 		flag = false;
 
 	inform->sorted = flag;
-    inform->first_elem = inform->a[inform->beg];
+	inform->first_elem = inform->a[inform->beg];
 	inform->last_elem = inform->a[inform->end];
 
 	_endthreadex(0);
@@ -47,40 +48,63 @@ unsigned __stdcall sorted_task(void* a)
 
 bool is_sorted_parallel(int* a)
 {
-    HANDLE TH[NTHREAD];
-    INFORM inform[NTHREAD];
-    size_t n = N / NTHREAD;
+	HANDLE TH[NTHREAD];
+	INFORM inform[NTHREAD];
+	size_t n = N / NTHREAD;
 
-    for (int i = 0; i < NTHREAD; i++)
-    {
-        inform[i].a = a;
-        inform[i].beg = i * n;
-        if (i == NTHREAD - 1)
-            inform[i].end = N - 1;
-        else
-            inform[i].end = (i + 1) * n - 1;
-        TH[i] = (HANDLE)_beginthreadex(nullptr, 0, &sorted_task, &inform[i], 0, nullptr);
-    }
-    WaitForMultipleObjects(NTHREAD, TH, true, INFINITE);
+	for (int i = 0; i < NTHREAD; i++)
+	{
+		inform[i].a = a;
+		inform[i].beg = i * n;
+		if (i == NTHREAD - 1)
+			inform[i].end = N - 1;
+		else
+			inform[i].end = (i + 1) * n - 1;
+		TH[i] = (HANDLE)_beginthreadex(nullptr, 0, &sorted_task, &inform[i], 0, nullptr);
+	}
+	WaitForMultipleObjects(NTHREAD, TH, true, INFINITE);
 
 
-    bool isSorted = inform[0].sorted;
-    for (size_t i = 1; i < NTHREAD && isSorted; i++)
-    {
-        isSorted *= inform[i].sorted;
-        isSorted *= inform[i - 1].last_elem < inform[i].first_elem;
-    }
+	bool isSorted = inform[0].sorted;
+	for (size_t i = 1; i < NTHREAD && isSorted; i++)
+	{
+		isSorted *= inform[i].sorted;
+		isSorted *= inform[i - 1].last_elem <= inform[i].first_elem;
+	}
 
-    for (size_t i = 0; i < NTHREAD; i++)
-        CloseHandle(TH[i]);
+	for (size_t i = 0; i < NTHREAD; i++)
+		CloseHandle(TH[i]);
 
-    return isSorted;
+	return isSorted;
+}
+
+void init_array(int* arr, bool random = true)
+{
+	if (random)
+	{
+		for (size_t i = 0; i < N; i++)
+			arr[i] = rand() % 100;
+	}
+	else
+	{
+		for (size_t i = 0; i < N; i++)
+			std::cin >> arr[i];
+	}
+}
+
+void print_array(int* a, int count)
+{
+	for (size_t i = 0; i < count; i++)
+		std::cout << a[i] << ' ';
+	std::cout << '\n';
 }
 
 int main()
 {
 	int* a = new int[N];
-	for (int i = 0; i < N; i++)
-		std::cin >> a[i];
+	srand(GetTickCount());
+	init_array(a, false);
+	//std::sort(a, a + N);
+	print_array(a, N);
 	std::cout << is_sorted_non_parallel(a) << '\n' << is_sorted_parallel(a) << '\n';
 }
